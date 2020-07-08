894804e8af9191a32cf3d7f170d559354ac351f7
dan <codecando.x@gmail.com>
2020-07-07 23:16:40 -0700

    started passing the flattened keys on each call to the recursive function so we don't use replace on every iteration

--- HierarchyAndStructure.py
+++ HierarchyAndStructure.py
@@ -1,11 +1,14 @@
 import types
+import json
 
 class HierarchyAndStructure:
 
+    __data = None
     __generated_object = None
     __quick_access_data = {}
-    __py_code_access_keys = []
-    __separator = None
+    __py_code_access_keys = {}
+    __separator = '.'
+    __index_identifier = 'i'
 
     __empties = {
         dict:'{}',
@@ -21,38 +24,49 @@ class HierarchyAndStructure:
         set:{}
     }
 
-    __templates = {
+    __direct_access_templates = {
         dict:"{}['{}']",
         list:"{}[{}]",
         tuple:"{}[{}]",
         set:"{}[{}]"
     }
 
-    __xtemplates = {
+    __obj_chain_templates = {
         dict:"{}X{}",
         list:"{}X{}",
         tuple:"{}X{}",
         set:"{}X{}"
     }
 
-    def __init__(self, data, separator = '.'):
+    def __init__(self, data = {}, file_path: str = '', separator: str = '.', index_identifier: str = 'i'):
+        self.__data = data
+        self.__file_path = file_path
         self.__separator = separator
+        self.__index_identifier = index_identifier
 
         """
         while we recursively traverse the the data object once, we do the following things:
-            - build a generated object hierarchy which one could access like(one.two.three.val), to get rid of the bracket access pattern
+            - build a generated object hierarchy which one could access like(one.two.three.val),
+                to get rid of the bracket access pattern
             - build a list of all key:val pairs which will enable direct lookup without recursing again
-            - build a list of all access code snippets, mostly used for discovery to find out available keys deep in the object hierarchy, also for looking up value deep down
+            - build a list of all access code snippets, mostly used for discovery to find out
+                available keys deep in the object hierarchy, also for looking up value deep down
 
         """
-        self.__generated_object = self.__build(data, types.SimpleNamespace(), 'X')
+        self.__generated_object = self.__build(self.__data, types.SimpleNamespace(), 'X', 'X')
 
         print("======================================\n")
-        print(self.__quick_access_data)
+        self.print_obj_access_keys()
         print("======================================\n")
-        print(self.__py_code_access_keys)
+        self.print_direct_access_keys()
 
     #def query(self, sql
+
+    def print_direct_access_keys(self):
+        print(json.dumps(self.__py_code_access_keys, indent=4))
+
+    def print_obj_access_keys(self):
+        print(json.dumps(self.__quick_access_data, indent=4))
 
     def get(self, key: str):
         if key in self.__quick_access_data:
@@ -64,79 +78,67 @@ class HierarchyAndStructure:
         will print all access keys/values to reach all values across object structure
     """
 
-    def __quick_access_key_py_access_key(self, quick_access_key: str):
-        quick_access_key = quick_access_key.replace("['", self.__separator)
-        quick_access_key = quick_access_key.replace("']", self.__separator)
-        quick_access_key = quick_access_key.replace("[", self.__separator)
-        quick_access_key = quick_access_key.replace("]", self.__separator)
+    def __build(self, current_obj, obj_hierarchy, direct_access_key_hierarchy: str = '', obj_chain_key_hierarchy: str = ''):
 
-        return quick_access_key
+        current_object_type = type(current_obj)
 
-    def __build(self, current, sobj, hier: str = ''):
-
-        current_type = type(current)
-
-        if current_type in self.__empties.keys():
-            empty = self.__empties[current_type]
-            if len(current) == 0:
-                print(hier + ' : ' + empty)
-
+        if current_object_type in self.__empties.keys():
+            empty = self.__empties[current_object_type]
+            if len(current_obj) == 0:
                 #add key:val pair to quick_access_data for easy flat retrieval latter
-
-                self.__quick_access_data[self.__quick_access_key_py_access_key(hier)] = empty
+                self.__quick_access_data[obj_chain_key_hierarchy] = empty
 
                 #add key to py_code_access_keys list so we can find out key paths for development
-                self.__py_code_access_keys.append(hier)
+                self.__py_code_access_keys[direct_access_key_hierarchy] = empty
 
-                nkey = hier
-                if type(nkey) is int:
-                    nkey = 'i' + str(nkey)
+                index_key = direct_access_key_hierarchy
+                #if type(index_key) is int:
+                if type(index_key) is int or index_key.isdigit() is True:
+                    index_key = 'i' + str(index_key)
 
-                setattr(sobj, nkey, self.__inits[current_type])
-                return sobj
+                setattr(obj_hierarchy, index_key, self.__inits[current_object_type])
+                return obj_hierarchy
 
             #this holds the keys which we will store in quick_access_data
-            xtemplate = self.__xtemplates[current_type].replace('X', self.__separator)
+            obj_chain_template = self.__obj_chain_templates[current_object_type].replace('X', self.__separator)
 
             #this holds the keys which we will store in py_code_access_keys
-            template = self.__templates[current_type]
+            direct_access_template = self.__direct_access_templates[current_object_type]
         else:
             empty = str(None)
 
-        if current_type is dict:
-            items = list(current.items())
-        elif current_type in [list, tuple, set]:
-            items = list(enumerate(current))
+        if current_object_type is dict:
+            items = list(current_obj.items())
+        elif current_object_type in [list, tuple, set]:
+            items = list(enumerate(current_obj))
 
         for key, val in items:
 
-            nkey = key
-            if type(nkey) is int or nkey.isdigit() is True:
-                nkey = 'i' + str(nkey)
-            #print('NKEY: ' + str(type(nkey)))
-            #print('NKEY VAL: ' + str(nkey))
+            index_key = key
+            if type(index_key) is int or index_key.isdigit() is True:
+                index_key = 'i' + str(index_key)
 
             val_type = type(val)
-            h = template.format(hier,str(key))
+            new_direct_access_key_hierarchy = direct_access_template.format(direct_access_key_hierarchy,str(key))
+            obj_chain_key_hierarchy = obj_chain_template.format(obj_chain_key_hierarchy,str(key))
             if val_type in [dict, list, tuple, set]:
-                new_obj_val = self.__build(val, types.SimpleNamespace(), h)
-                setattr(sobj, nkey, new_obj_val)
+                new_obj_val = self.__build(val, types.SimpleNamespace(), new_direct_access_key_hierarchy, obj_chain_key_hierarchy)
+                setattr(obj_hierarchy, index_key, new_obj_val)
             else:
-                v = str(val)
-                if val_type is str and len(v) == 0:
-                    v = '""'
+                new_val = str(val)
+                if val_type is str and len(new_val) == 0:
+                    new_val = '""'
 
-                print("{} : {}".format(h, v))
+                #print("{} : {}".format(new_direct_access_key_hierarchy, new_val))
+
                 #add key:val pair to quick_access_data for easy flat retrieval latter
-                self.__quick_access_data[self.__quick_access_key_py_access_key(h)] = v
+                self.__quick_access_data[obj_chain_key_hierarchy] = new_val
 
-                self.__py_code_access_keys.append(h)
+                self.__py_code_access_keys[new_direct_access_key_hierarchy] = new_val
 
-                new_obj_val = v
-                #print('NKEY: ' + str(type(nkey)))
-                setattr(sobj, nkey, new_obj_val)
+                setattr(obj_hierarchy, index_key, new_val)
 
-        return sobj
+        return obj_hierarchy
 
     '''
     def query
--- hands.py
+++ hands.py
@@ -1,74 +1,31 @@
-
-'''
-
-cat file.json | hands.py -s z.one.two.three
-cat file.json | hands.py -q "select * from cities"
-hands.py -s file.json -s z.one.two.three
-
-#prints access keys of how they would be used as parameters to -s
-hands.py -s file.json -k
-
-we could write something similar in javascript and create a UI for it under jsonsql.co
-where people can paste in or load json files and then query them easier
-for prototyping and getting fast insights from json datasets
-
-'''
-
 import json
 import sys
-import types
+import argparse
 
 from TestHierarchyData import TestHierarchyData
 from HierarchyAndStructure import HierarchyAndStructure
 
-data  = TestHierarchyData().sample()
+arg_parser = argparse.ArgumentParser()
 
-print(data)
-#print(json.dumps(test_hierarchy, indent=4))
+arg_parser.add_argument("--get")
+arg_parser.add_argument("--query")
+arg_parser.add_argument("--source")
+arg_parser.add_argument("--values")
+arg_parser.add_argument("--obj_keys")
+arg_parser.add_argument("--py_keys")
 
-#data = json.load(sys.stdin)
+args = arg_parser.parse_args()
 
-'''
-def access_keys(self, current, hier: str = ''):
+print(args)
 
-    current_type = type(current)
+data = None
+filepath = None
 
-    if current_type in self.empties.keys():
-        empty = self.empties[current_type]
-        if len(current) == 0:
-            print(hier + ' : ' + empty)
-            return
-        template = self.templates[current_type]
-    else:
-        empty = str(None)
+#piped input takes precedence over file source
+if not sys.stdin.isatty():
+    data = json.load(sys.stdin)
+elif args.source is not None and len(args.source) > 0:
+    filepath = args.source
 
-    if current_type is dict:
-        items = list(current.items())
-    elif current_type in [list, tuple, set]:
-        items = list(enumerate(current))
-
-    for key, val in items:
-        val_type = type(val)
-        h = template.format(hier,str(key))
-        if val_type in [dict, list, tuple, set]:
-            self.access_keys(val, h)
-        else:
-            v = str(val)
-            if val_type is str and len(v) == 0:
-                v = '""'
-
-            print("{} : {}".format(h, v))
-'''
-
-hands = HierarchyAndStructure(data)
-
-print(hands.get('X.t.0'))
-
-'''
-print(obj.Z.one.two.three)
-print(obj.i2)
-print(obj.s.i0)
-print(obj.s.i3.i0)
-print(obj.i1.i0)
-print(obj.i1.i4.one)
-'''
+if data is not None or filepath is not None:
+    hands = HierarchyAndStructure(data, filepath)
